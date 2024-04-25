bfba1402a78672f23bbe90e92a90c3a2bd7bda14
Georg <48655643+gmgeorg@users.noreply.github.com>
2024-04-24 22:58:29 -0400

    add avg outcome and metrics (#6)

--- pypsps/keras/metrics.py
+++ pypsps/keras/metrics.py
@@ -5,23 +5,13 @@ from pypsps import utils
 
 
 @tf.keras.utils.register_keras_serializable(package="pypsps")
-def propensity_score_crossentropy(y_true: tf.Tensor, y_pred: tf.Tensor):
-    """Computes cross entropy for the propensity score. Used as a metric in pypsps model."""
-    _, _, _, propensity_score = utils.split_y_pred(y_pred)
-    treatment_true = y_true[:, 0:1]
-    return tf.keras.metrics.binary_crossentropy(
-        y_true=treatment_true, y_pred=propensity_score
-    )
-
-
-@tf.keras.utils.register_keras_serializable(package="pypsps")
 class PropensityScoreBinaryCrossentropy(tf.keras.metrics.BinaryCrossentropy):
     """Computes cross entropy for the propensity score. Used as a metric in pypsps model."""
 
     def update_state(self, y_true, y_pred, sample_weight=None):
         """Updates state."""
         _, _, _, propensity_score = utils.split_y_pred(y_pred)
-        treatment_true = y_true[:, 0:1]
+        treatment_true = y_true[:, 1:]
         super().update_state(
             y_true=treatment_true, y_pred=propensity_score, sample_weight=sample_weight
         )
@@ -34,7 +24,33 @@ class PropensityScoreAUC(tf.keras.metrics.AUC):
     def update_state(self, y_true, y_pred, sample_weight=None):
         """Updates state"""
         _, _, _, propensity_score = utils.split_y_pred(y_pred)
-        treatment_true = y_true[:, 0:1]
+        treatment_true = y_true[:, 1:]
         super().update_state(
             y_true=treatment_true, y_pred=propensity_score, sample_weight=sample_weight
+        )
+
+
+@tf.keras.utils.register_keras_serializable(package="pypsps")
+class OutcomeMeanSquaredError(tf.keras.metrics.MeanSquaredError):
+    """MSE computed on the ouptut for weighted average outcome prediction."""
+
+    def update_state(self, y_true, y_pred, sample_weight=None):
+        """Updates state"""
+        avg_outcome = utils.agg_outcome_pred(y_pred)
+        outcome_true = y_true[:, 0:1]
+        super().update_state(
+            y_true=outcome_true, y_pred=avg_outcome, sample_weight=sample_weight
+        )
+
+
+@tf.keras.utils.register_keras_serializable(package="pypsps")
+class OutcomeMeanAbsoluteError(tf.keras.metrics.MeanAbsoluteError):
+    """MSE computed on the ouptut for weighted average outcome prediction."""
+
+    def update_state(self, y_true, y_pred, sample_weight=None):
+        """Updates state"""
+        avg_outcome = utils.agg_outcome_pred(y_pred)
+        outcome_true = y_true[:, 0:1]
+        super().update_state(
+            y_true=outcome_true, y_pred=avg_outcome, sample_weight=sample_weight
         )
