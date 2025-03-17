fix broadcasting and binary loss errors (#11)

pypsps/tests/test_models.py
@@ -1,35 +1,33 @@
 """Test module for model functions."""
 
 import numpy as np
-import pytest
 import tensorflow as tf
-import random
 
 from .. import datasets
 from ..keras import models
-
 
 tfk = tf.keras
 
 
 def test_build_toy_model():
+    """test toy model"""
     np.random.seed(10)
     ks_data = datasets.KangSchafer(true_ate=10).sample(n_samples=1000)
 
     inputs, outputs = ks_data.to_keras_inputs_outputs()
     tf.random.set_seed(10)
-    model = models.build_toy_model(
-        n_states=3, n_features=ks_data.n_features, compile=True
-    )
+    model = models.build_toy_model(n_states=3, n_features=ks_data.n_features, compile=True)
     preds = model.predict(inputs)
+    assert preds.shape[0] == ks_data.n_samples
     assert not np.isnan(preds.sum().sum())
 
 
 def test_build_model():
+    """test build models"""
     np.random.seed(10)
     ks_data = datasets.KangSchafer(true_ate=10).sample(n_samples=1000)
 
-    inputs, outputs = ks_data.to_keras_inputs_outputs()
+    inputs, _ = ks_data.to_keras_inputs_outputs()
     tf.random.set_seed(10)
     model = models.build_model_binary_normal(
         n_states=3,
@@ -41,4 +39,5 @@ def test_build_model():
         scale_layer=(10, "tanh"),
     )
     preds = model.predict(inputs)
+    assert preds.shape[0] == ks_data.n_samples
     assert not np.isnan(preds.sum().sum())

pypsps/tests/test_neglogliks.py
@@ -8,7 +8,6 @@ import tensorflow_probability as tfp
 
 from ..keras import neglogliks
 
-
 tfd = tfp.distributions
 
 
@@ -19,18 +18,17 @@ def _test_data() -> Tuple[np.ndarray, np.ndarray]:
 
 
 @pytest.mark.parametrize(
-    "reduction,expected_len",
-    [("sum", 1), ("sum_over_batch_size", 1), ("none", 3)],  # ("auto", 1),
+    "reduction,expected_shape",
+    [("sum", ()), ("sum_over_batch_size", ()), ("none", (3,))],  # ("auto", 1),
 )
-def test_negloglik_normal_loss(reduction, expected_len):
+def test_negloglik_normal_loss(reduction, expected_shape):
     y_true, y_pred = _test_data()
     loss = neglogliks.NegloglikNormal(reduction=reduction)(
         y_true=y_true.astype("float32"), y_pred=y_pred.astype("float32")
     )
-    if expected_len == 1:
-        assert not len(loss.numpy().shape)
-    else:
-        assert loss.shape[0] == expected_len
+    print(loss.shape)
+
+    assert loss.shape == expected_shape
 
 
 @pytest.mark.parametrize(

pypsps/utils.py
@@ -68,10 +68,12 @@ def split_outcome_pred(
         return tf.split(outcome_pred, n_outcome_pred_cols, axis=1)
 
 
-def split_y_true(y_true: _Y_PRED_DTYPE, n_outcome_cols: int) -> Tuple[_Y_PRED_DTYPE, _Y_PRED_DTYPE]:
+def split_y_true(
+    y_true: _Y_PRED_DTYPE, n_outcome_true_cols: int
+) -> Tuple[_Y_PRED_DTYPE, _Y_PRED_DTYPE]:
     """Splits y_true = (outcome, treatment) into separate tensors."""
-    outcome_true = y_true[:, :n_outcome_cols]
-    treatment_true = y_true[:, n_outcome_cols:]
+    outcome_true = y_true[:, :n_outcome_true_cols]
+    treatment_true = y_true[:, n_outcome_true_cols:]
     return outcome_true, treatment_true
 
 