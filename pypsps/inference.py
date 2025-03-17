fix broadcasting and binary loss errors

pypsps/inference.py
@@ -4,11 +4,12 @@ Inference for now only supports binary treatment (i.e., switch between 0 / 1 to 
 counterfactual predictions).
 """
 
-from typing import Any, Union
+from typing import Any, List, Optional, Union
 
 import numpy as np
 import pandas as pd
 import tensorflow as tf
+import tqdm
 
 from . import utils
 
@@ -38,10 +39,10 @@ def predict_counterfactual(
     Returns:
       Model predictions for Pr(Y | X, do(T))
     """
-    return model.predict([features, treatment])
+    return model.predict([features, treatment], verbose=0)
 
 
-def predict_ute(model: tf.keras.Model, features: Any) -> Union[pd.Series, np.ndarray]:
+def predict_ute_binary(model: tf.keras.Model, features: Any) -> Union[pd.Series, np.ndarray]:
     """Predicts unit-level treatment effect for binary treatment.
 
     Args:
@@ -75,6 +76,88 @@ def predict_ute(model: tf.keras.Model, features: Any) -> Union[pd.Series, np.nda
     return weighted_ute
 
 
-def predict_ate(model: tf.keras.Model, features: pd.DataFrame) -> float:
+def predict_ate_binary(model: tf.keras.Model, features: pd.DataFrame) -> float:
     """Computes average treatment effect as averaging UTE estimates."""
-    return predict_ute(model, features).mean()
+    return predict_ute_binary(model, features).mean()
+
+
+def predict_ute_continuous(
+    model: tf.keras.Model,
+    features: Any,
+    treatment_grid: List[float],
+    baseline_treatment: Optional[float] = None,
+) -> Union[pd.DataFrame, np.ndarray]:
+    """Predicts unit-level treatment effect for continuous treatment.
+
+    Args:
+      model: a trained pypsps model.
+      features: features (X) for the causal model. Often a
+        pd.DataFrame/np.ndarray, but can also be non-standard
+        data structure as long as the pypsps model can use it as
+        input to model.predict([features, ...]).
+      treatment_grid: grid of treatment values to get UTE for.  This will be the number of columns
+        in the resulting array / dataframe
+      baseline_treatment: what's the baseline treatment value.  If None, uses the avg of the treatment_grid.
+
+    Returns:
+      A pd.DataFrame (if features is a DataFrame) or a np.ndarray of same number of
+      rows as features; number of columns is the number of treatment values on the grid.
+    """
+
+    if baseline_treatment is None:
+        baseline_treatment = np.mean(np.array(treatment_grid))
+
+    n_samples = features.shape[0]
+    base_treatment = np.full((n_samples, 1), baseline_treatment)
+
+    base_preds = predict_counterfactual(model, features, base_treatment)
+
+    o_base, weights_base, t_base = utils.split_y_pred(
+        base_preds,
+        n_outcome_pred_cols=model.loss._outcome_loss._n_outcome_pred_cols,
+        n_treatment_pred_cols=model.loss._outcome_loss._n_treatment_pred_cols,
+    )
+
+    utes = []
+    for c in tqdm.tqdm(treatment_grid):
+        c_treatment = np.full((n_samples, 1), c)
+        c_preds = predict_counterfactual(model, features, c_treatment)
+        o_c, _, _ = utils.split_y_pred(
+            c_preds,
+            n_outcome_pred_cols=model.loss._outcome_loss._n_outcome_pred_cols,
+            n_treatment_pred_cols=model.loss._outcome_loss._n_treatment_pred_cols,
+        )
+        c_ute = o_c - o_base
+        # Weighted average across states.
+        c_ute_weighted = (weights_base * c_ute).sum(axis=1)
+        utes.append(c_ute_weighted)
+
+    utes = np.array(utes).transpose()
+    if isinstance(features, pd.DataFrame):
+        return pd.DataFrame(utes, index=features.index, columns=treatment_grid)
+    return utes
+
+
+def predict_ate_continuous(
+    model: tf.keras.Model,
+    features: Any,
+    treatment_grid: List[float],
+    baseline_treatment: Optional[float] = None,
+) -> Union[pd.Series, np.ndarray]:
+    """Computes the average treatment effect (ATE) for continous treatment.
+
+    Args:
+      model: a trained pypsps model.
+      features: features (X) for the causal model. Often a
+        pd.DataFrame/np.ndarray, but can also be non-standard
+        data structure as long as the pypsps model can use it as
+        input to model.predict([features, ...]).
+      treatment_grid: grid of treatment values to get UTE for.  This will be the number of columns
+        in the resulting array / dataframe
+      baseline_treatment: what's the baseline treatment value.  If None, uses the avg of the treatment_grid.
+
+    Returns:
+      A pd.Series (if features is a DataFrame) or a np.ndarray of same number of
+      rows as the number of values in treatment grid.
+    """
+    return predict_ute_continuous(model, features, treatment_grid, baseline_treatment).mean(axis=0)

pypsps/keras/losses.py
@@ -8,6 +8,10 @@ import tensorflow as tf
 from .. import utils
 
 
+def _gen_col_selectors(x: int, n: int, k: int) -> list:
+    return [x + i * n for i in range(k)]
+
+
 @tf.keras.utils.register_keras_serializable(package="pypsps")
 class OutcomeLoss(tf.keras.losses.Loss):
     """Computes outcome loss for a pypsps model with multi-output predictions.
@@ -19,20 +23,26 @@ class OutcomeLoss(tf.keras.losses.Loss):
     def __init__(
         self,
         loss: tf.keras.losses.Loss,
+        n_outcome_true_cols: int,
         n_outcome_pred_cols: int,
-        n_treatment_pred_cols: int = 1,
+        n_treatment_pred_cols: int,
         **kwargs,
     ):
         """Initializes the outcome loss.
 
         Args:
-          outcome_loss: a keras loss function with NONE reduction (ie element-wise).
-            This is a requirement to properly computed the weighted loss across states.
+          loss: a keras loss function with NONE reduction (ie element-wise).  This is a requirement to properly computed the
+            weighted loss across states.
+          n_outcome_true_cols: number of outcome columns in y_true.  Used to split outcome_true and treatment_true.
+          n_outcome_pred_cols: number of outcome columns in y_pred.
+          n_treatment_pred_cols: number of treatment columns in y_pred.
+          **kwargs: additional arguments passed to keras Loss class.
         """
         super().__init__(**kwargs)
         assert isinstance(loss, tf.keras.losses.Loss)
         assert loss.reduction == tf.keras.losses.Reduction.NONE
         self._loss = loss
+        self._n_outcome_true_cols = n_outcome_true_cols
         self._n_outcome_pred_cols = n_outcome_pred_cols
         self._n_treatment_pred_cols = n_treatment_pred_cols
 
@@ -51,37 +61,32 @@ class OutcomeLoss(tf.keras.losses.Loss):
             y_pred, self._n_outcome_pred_cols, self._n_treatment_pred_cols
         )
 
-        outcome_param_preds = utils.split_outcome_pred(
-            outcome_params_pred, n_outcome_pred_cols=self._n_outcome_pred_cols
-        )
-
-        n_cols_true = utils.get_n_cols(y_true)
-        outcome_true = utils.split_y_true(y_true, n_cols_true - 1)[0]
-
+        outcome_true = utils.split_y_true(y_true, self._n_outcome_true_cols)[0]
         weighted_loss = 0.0
         for j in range(n_states):
-            outcome_pred_state_j_list = []
-            for params_pred in outcome_param_preds:
-                outcome_pred_state_j_list.append(params_pred[:, j])
-            outcome_pred_state_j = tf.stack(outcome_pred_state_j_list, axis=1)
-
-            weighted_loss += weights[:, j] * self._loss(
+            cols_to_select = _gen_col_selectors(j, n_states, self._n_outcome_pred_cols)
+            outcome_pred_state_j = tf.gather(outcome_params_pred, cols_to_select, axis=1)
+            loss_state_j = self._loss(
                 y_true=outcome_true,
                 y_pred=outcome_pred_state_j,
             )
+            weighted_loss += weights[:, j] * loss_state_j
 
         if self.reduction == tf.keras.losses.Reduction.NONE:
             return weighted_loss
 
-        weighted_loss = tf.reduce_sum(weighted_loss)
+        weighted_loss_sum = tf.reduce_sum(weighted_loss)
         if self.reduction in (tf.keras.losses.Reduction.SUM,):
-            return weighted_loss
+            return weighted_loss_sum
 
         # Divide by batch sample size; note that sum of all weights = n_samples
         # since weights are softmax per row.
-        if self.reduction in (tf.keras.losses.Reduction.SUM_OVER_BATCH_SIZE):
-            weighted_loss /= tf.reduce_sum(weights)
-            return weighted_loss
+        if self.reduction in (
+            tf.keras.losses.Reduction.AUTO,
+            tf.keras.losses.Reduction.SUM_OVER_BATCH_SIZE,
+        ):
+            weighted_loss_avg = weighted_loss_sum / tf.reduce_sum(weights)
+            return weighted_loss_avg
 
         raise NotImplementedError("self.reduction='%s' is not implemented", self.reduction)
 
@@ -93,21 +98,37 @@ class TreatmentLoss(tf.keras.losses.Loss):
     def __init__(
         self,
         loss: tf.keras.losses.Loss,
-        n_outcome_pred_cols: int = 2,
-        n_treatment_pred_cols: int = 1,
+        n_outcome_true_cols: int,
+        n_outcome_pred_cols: int,
+        n_treatment_pred_cols: int,
         **kwargs,
     ):
+        """Initializes class.
+
+        Args:
+          loss: a keras loss function with NONE reduction (ie element-wise).
+          n_outcome_true_cols: number of outcome columns in y_true.  Used to split outcome_true and treatment_true.
+          n_outcome_pred_cols: number of outcome columns in y_pred.
+          n_treatment_pred_cols: number of treatment columns in y_pred.
+        """
+
         super().__init__(**kwargs)
         self._loss = loss
+        self._n_outcome_true_cols = n_outcome_true_cols
         self._n_outcome_pred_cols = n_outcome_pred_cols
         self._n_treatment_pred_cols = n_treatment_pred_cols
 
     def call(self, y_true, y_pred):
         """Evaluates loss on treatment label and predicted treatment of y_pred (propensity score)."""
-        return self._loss(
-            y_true[:, 1],
-            utils.split_y_pred(y_pred, self._n_outcome_pred_cols, self._n_treatment_pred_cols)[-1],
+        _, treat_true = utils.split_y_true(y_true, n_outcome_true_cols=self._n_outcome_true_cols)
+        _, _, treat_preds = utils.split_y_pred(
+            y_pred, self._n_outcome_pred_cols, self._n_treatment_pred_cols
         )
+        loss = self._loss(
+            y_true=treat_true,
+            y_pred=treat_preds,
+        )
+        return loss
 
 
 @tf.keras.utils.register_keras_serializable(package="pypsps")
@@ -184,7 +205,7 @@ class CausalLoss(tf.keras.losses.Loss):
         total_loss = self._outcome_loss_weight * loss_outcome + self._alpha * loss_treatment
         if self._predictive_states_regularizer is not None:
             weights = utils.split_y_pred(
-                y_pred,
+                y_pred=y_pred,
                 n_outcome_pred_cols=self._outcome_loss._n_outcome_pred_cols,
                 n_treatment_pred_cols=self._outcome_loss._n_treatment_pred_cols,
             )[1]

pypsps/keras/metrics.py
@@ -37,13 +37,36 @@ class PropensityScoreAUC(tf.keras.metrics.AUC):
 
 
 @tf.keras.utils.register_keras_serializable(package="pypsps")
+class TreatmentMeanSquaredError(tf.keras.metrics.MeanSquaredError):
+    """MSE computed on continuous treatment prediction."""
+
+    def update_state(self, y_true, y_pred, sample_weight=None):
+        """Updates state"""
+        treat_pred = utils.split_y_pred(y_pred, n_outcome_pred_cols=1, n_treatment_pred_cols=2)[2]
+        treat_true = utils.split_y_true(y_true, n_outcome_true_cols=1)[1]
+        super().update_state(y_true=treat_true, y_pred=treat_pred, sample_weight=sample_weight)
+
+
+# TODO: add unit test for Outcome* metrics
+@tf.keras.utils.register_keras_serializable(package="pypsps")
+class TreatmentMeanAbsoluteError(tf.keras.metrics.MeanAbsoluteError):
+    """MSE computed on the ouptut for weighted average outcome prediction."""
+
+    def update_state(self, y_true, y_pred, sample_weight=None):
+        """Updates state"""
+        treat_pred = utils.split_y_pred(y_pred, n_outcome_pred_cols=1, n_treatment_pred_cols=2)[2]
+        treat_true = utils.split_y_true(y_true, n_outcome_true_cols=1)[1]
+        super().update_state(y_true=treat_true, y_pred=treat_pred, sample_weight=sample_weight)
+
+
+@tf.keras.utils.register_keras_serializable(package="pypsps")
 class OutcomeMeanSquaredError(tf.keras.metrics.MeanSquaredError):
     """MSE computed on the ouptut for weighted average outcome prediction."""
 
     def update_state(self, y_true, y_pred, sample_weight=None):
         """Updates state"""
         avg_outcome = utils.agg_outcome_pred(y_pred, n_outcome_pred_cols=2, n_treatment_pred_cols=1)
-        outcome_true = utils.split_y_true(y_true, n_outcome_cols=1)[0]
+        outcome_true = utils.split_y_true(y_true, n_outcome_true_cols=1)[0]
         super().update_state(y_true=outcome_true, y_pred=avg_outcome, sample_weight=sample_weight)
 
 
@@ -55,7 +78,7 @@ class OutcomeMeanAbsoluteError(tf.keras.metrics.MeanAbsoluteError):
     def update_state(self, y_true, y_pred, sample_weight=None):
         """Updates state"""
         avg_outcome = utils.agg_outcome_pred(y_pred, n_outcome_pred_cols=2, n_treatment_pred_cols=1)
-        outcome_true = utils.split_y_true(y_true, n_outcome_cols=1)[0]
+        outcome_true = utils.split_y_true(y_true, n_outcome_true_cols=1)[0]
         super().update_state(y_true=outcome_true, y_pred=avg_outcome, sample_weight=sample_weight)
 
 
@@ -74,5 +97,5 @@ def _tr_kernel(weights: tf.Tensor) -> tf.Tensor:
 def predictive_state_df(y_true, y_pred) -> tf.Tensor:
     """Computes degrees of freedom of predictive state weights."""
     del y_true
-    _, weights, _ = utils.split_y_pred(y_pred, n_outcome_pred_cols=1, n_treatment_pred_cols=2)
+    _, weights, _ = utils.split_y_pred(y_pred, n_outcome_pred_cols=1, n_treatment_pred_cols=1)
     return _tr_kernel(weights)

pypsps/keras/models.py
@@ -14,20 +14,22 @@ tfk = tf.keras
 _EPS = 1e-3
 
 
-def _build_binary_continuous_causal_loss(
+def _build_binary_normal_causal_loss(
     n_states: int,
     alpha: float,
     df_penalty_l1: float,
 ) -> losses.CausalLoss:
-    """Builds an example of binary treatment & continuous outcome causal loss."""
+    """Builds an example of binary treatment & Normal outcome causal loss."""
     psps_outcome_loss = losses.OutcomeLoss(
         loss=neglogliks.NegloglikNormal(reduction="none"),
+        n_outcome_true_cols=1,
         n_outcome_pred_cols=2,
         n_treatment_pred_cols=1,
         reduction="sum_over_batch_size",
     )
     psps_treat_loss = losses.TreatmentLoss(
         loss=tf.keras.losses.BinaryCrossentropy(reduction="none"),
+        n_outcome_true_cols=1,
         n_outcome_pred_cols=2,
         n_treatment_pred_cols=1,
         reduction="sum_over_batch_size",
@@ -74,18 +76,20 @@ def build_toy_model(
     assert n_states >= 1, f"Got n_states={n_states}"
     assert n_features >= 1, f"Got n_features={n_features}"
 
-    features = tfk.layers.Input(shape=(n_features,))
-    treat = tfk.layers.Input(shape=(1,))
+    features = tfk.layers.Input(shape=(n_features,), name="features")
+    treat = tfk.layers.Input(shape=(1,), name="treatment")
 
-    features_bn = tfk.layers.BatchNormalization()(features)
-    feat_treat = tfk.layers.Concatenate(name="features_and_treatment")([features_bn, treat])
+    features_bn = tfk.layers.BatchNormalization(name="features_bn")(features)
+    feat_treat = tfk.layers.Concatenate(name="features_bn_and_treatment")([features_bn, treat])
 
     ps_hidden = tfk.layers.Dense(10, "relu")(features_bn)
     ps_hidden = tfk.layers.BatchNormalization()(ps_hidden)
     ps_hidden = tfk.layers.Dropout(0.2)(ps_hidden)
     ps_hidden = tfk.layers.Dense(10, "selu")(ps_hidden)
 
-    ps_hidden = tf.keras.layers.Concatenate()([ps_hidden, features_bn])
+    ps_hidden = tf.keras.layers.Concatenate(name="ps_hidden_and_features_bn")(
+        [ps_hidden, features_bn]
+    )
     pss = pypress.keras.layers.PredictiveStateSimplex(n_states=n_states, input_dim=n_features)
     pred_states = pss(ps_hidden)
     # Propensity score for binary treatment (--> "sigmoid" activation).
@@ -97,14 +101,16 @@ def build_toy_model(
     outcome_hidden = tf.keras.layers.Dropout(0.2)(outcome_hidden)
     outcome_hidden = tf.keras.layers.BatchNormalization()(outcome_hidden)
 
-    outcome_hidden = tf.keras.layers.Concatenate()([outcome_hidden, feat_treat])
+    outcome_hidden = tf.keras.layers.Concatenate(name="outcome_hidden_and_ft")(
+        [outcome_hidden, feat_treat]
+    )
 
-    outcome_preds = []
+    loc_preds = []
     scale_preds = []
     # One outcome model per state.
     for state_id in range(n_states):
-        outcome_preds.append(
-            tfk.layers.Dense(1, name="outcome_pred_state_" + str(state_id))(
+        loc_preds.append(
+            tfk.layers.Dense(1, name="loc_pred_state_" + str(state_id))(
                 tfk.layers.Dense(5, "selu", name="feat_eng_state_" + str(state_id))(outcome_hidden)
             )
         )
@@ -117,17 +123,18 @@ def build_toy_model(
             )
         )
 
-    outcome_comb = tfk.layers.Concatenate(name="outcome_pred_combined")(outcome_preds)
+    loc_comb = tfk.layers.Concatenate(name="loc_pred_combined")(loc_preds)
     scale_comb = tfk.layers.Concatenate(name="scale_pred_combined")(scale_preds)
 
+    outcome_pred = tfk.layers.Concatenate(name="loc_scale_pred")([loc_comb, scale_comb])
     outputs_concat = tfk.layers.Concatenate(name="output_tensor")(
-        [outcome_comb, scale_comb, pred_states, prop_score]
+        [outcome_pred, pred_states, prop_score]
     )
 
     model = tfk.models.Model(inputs=[features, treat], outputs=outputs_concat)
 
     if compile:
-        psps_causal_loss = _build_binary_continuous_causal_loss(
+        psps_causal_loss = _build_binary_normal_causal_loss(
             n_states=n_states,
             alpha=alpha,
             df_penalty_l1=df_penalty_l1,
@@ -263,7 +270,7 @@ def build_model_binary_normal(
     model = tfk.models.Model(inputs=[features, treat], outputs=outputs_concat)
 
     if compile:
-        psps_causal_loss = _build_binary_continuous_causal_loss(
+        psps_causal_loss = _build_binary_normal_causal_loss(
             n_states=n_states,
             alpha=alpha,
             df_penalty_l1=df_penalty_l1,

pypsps/keras/neglogliks.py
@@ -2,11 +2,15 @@
 
 import math
 
-import numpy as np
 import tensorflow as tf
 import tensorflow_probability as tfp
 
+import pypsps.utils
+
 tfd = tfp.distributions
+
+
+_EPS = 1e-6
 
 
 @tf.keras.utils.register_keras_serializable(package="pypsps")
@@ -22,10 +26,7 @@ class NegloglikLoss(tf.keras.losses.Loss):
 
     def call(self, y_true, y_pred):
         """Implements the loss function call."""
-        if isinstance(y_pred, np.ndarray):
-            n_params = y_pred.shape[1]
-        else:
-            n_params = y_pred.get_shape().as_list()[1]
+        n_params = pypsps.utils.get_n_cols(y_pred)
 
         y_pred_cols = [tf.squeeze(c) for c in tf.split(y_pred, n_params, axis=1)]
         distr = self._distribution_constructor(*y_pred_cols)
@@ -43,11 +44,11 @@ class NegloglikLoss(tf.keras.losses.Loss):
         raise NotImplementedError("reduction='%s' is not implemented", self.reduction)
 
 
-def _negloglik_normal(y: tf.Tensor, mu: tf.Tensor, sigma: tf.Tensor) -> tf.Tensor:
+def _negloglik_normal(y: tf.Tensor, loc: tf.Tensor, scale: tf.Tensor) -> tf.Tensor:
     """Computes negative log-likelihood of data y ~ Normal(mu, sigma)."""
-    negloglik_element = tf.math.log(2.0 * math.pi) / 2.0 + tf.math.log(sigma)
-    negloglik_element += 0.5 * tf.square((y - mu) / sigma)
-    return negloglik_element
+    negloglik_element = tf.math.log(2.0 * math.pi) / 2.0 + tf.math.log(scale + _EPS)
+    negloglik_element += 0.5 * tf.square((y - loc) / (scale + _EPS))
+    return tf.squeeze(negloglik_element)
 
 
 @tf.keras.utils.register_keras_serializable(package="pypsps")
@@ -56,17 +57,17 @@ class NegloglikNormal(tf.keras.losses.Loss):
 
     def call(self, y_true, y_pred):
         """Implements the loss function call."""
-        y_pred_mu = y_pred[:, 0]
-        y_pred_scale = y_pred[:, 1]
-
-        losses = _negloglik_normal(y_true, y_pred_mu, y_pred_scale)
+        y_true = tf.squeeze(y_true)
+        loc_pred = y_pred[:, 0]
+        scale_pred = y_pred[:, 1]
+        losses = _negloglik_normal(y_true, loc_pred, scale_pred)
         if self.reduction == tf.keras.losses.Reduction.NONE:
             return losses
         if self.reduction == tf.keras.losses.Reduction.SUM:
-            return tf.reduce_sum(losses)
+            return tf.reduce_sum(losses, axis=-1)
         if self.reduction in (
             tf.keras.losses.Reduction.SUM_OVER_BATCH_SIZE,
             tf.keras.losses.Reduction.AUTO,
         ):
-            return tf.reduce_mean(losses)
+            return tf.reduce_mean(losses, axis=-1)
         raise NotImplementedError("reduction='%s' is not implemented", self.reduction)

pypsps/tests/test_losses.py
@@ -12,35 +12,13 @@ from ..keras import losses, models, neglogliks
 tfk = tf.keras
 
 
-def test_psps_model_and_causal_loss():
+def test_psps_model_build_and_predict():
+    """Test build & predict"""
     tf.random.set_seed(0)
     random.seed(0)
     np.random.seed(0)
 
-    pypsps_outcome_loss = losses.OutcomeLoss(
-        loss=neglogliks.NegloglikNormal(reduction="none"),
-        n_outcome_pred_cols=2,
-        n_treatment_pred_cols=1,
-        reduction="sum_over_batch_size",
-    )
-
-    pypsps_treat_loss = losses.TreatmentLoss(
-        loss=tf.keras.losses.BinaryCrossentropy(reduction="none"),
-        n_outcome_pred_cols=2,
-        n_treatment_pred_cols=1,
-        reduction="sum_over_batch_size",
-    )
-    pypsps_causal_loss = losses.CausalLoss(
-        outcome_loss=pypsps_outcome_loss,
-        treatment_loss=pypsps_treat_loss,
-        alpha=1.0,
-        outcome_loss_weight=0.0,
-        predictive_states_regularizer=tf.keras.regularizers.l2(0.1),
-        reduction="sum_over_batch_size",
-    )
-
-    np.random.seed(10)
-    ks_data = datasets.KangSchafer(true_ate=10).sample(n_samples=1000)
+    ks_data = datasets.KangSchafer(true_ate=10, seed=10).sample(n_samples=1000)
 
     inputs, outputs = ks_data.to_keras_inputs_outputs()
     assert outputs.shape == (1000, 2)
@@ -53,20 +31,66 @@ def test_psps_model_and_causal_loss():
     assert outcome_params_pred.shape == (1000, 3 * 2)  # (obs, states * 2) for (loc, scale)
     assert propensity_score.shape[0] == 1000
     assert weights.shape == (1000, 3)
+
+
+@pytest.mark.parametrize(
+    "reduction,expected_shape",
+    [("sum", ()), ("sum_over_batch_size", ()), ("none", (5,))],  # ("auto", 1),
+)
+def test_psps_causal_loss(reduction, expected_shape):
+    """Test psps causal loss"""
+    tf.random.set_seed(0)
+    random.seed(0)
+    np.random.seed(0)
+
+    pypsps_outcome_loss = losses.OutcomeLoss(
+        loss=neglogliks.NegloglikNormal(reduction="none"),
+        n_outcome_true_cols=1,
+        n_outcome_pred_cols=2,
+        n_treatment_pred_cols=1,
+        reduction=reduction,
+    )
+
+    pypsps_treat_loss = losses.TreatmentLoss(
+        loss=tf.keras.losses.BinaryCrossentropy(reduction="none"),
+        n_outcome_true_cols=1,
+        n_outcome_pred_cols=2,
+        n_treatment_pred_cols=1,
+        reduction=reduction,
+    )
+    pypsps_causal_loss = losses.CausalLoss(
+        outcome_loss=pypsps_outcome_loss,
+        treatment_loss=pypsps_treat_loss,
+        alpha=1.0,
+        outcome_loss_weight=1.0,
+        predictive_states_regularizer=tf.keras.regularizers.l2(0.1),
+        reduction=reduction,
+    )
+
+    ks_data = datasets.KangSchafer(true_ate=10, seed=10).sample(n_samples=5)
+
+    inputs, outputs = ks_data.to_keras_inputs_outputs()
+    assert outputs.shape == (5, 2)
+
+    tf.random.set_seed(10)
+    model = models.build_toy_model(n_states=3, n_features=ks_data.n_features, compile=True)
+    preds = model.predict(inputs)
+
     causal_loss = pypsps_causal_loss(outputs, preds)
-    assert causal_loss.numpy() == pytest.approx(34.02, 0.01)
+    print(reduction, causal_loss)
+    assert causal_loss.shape == expected_shape
 
 
 def test_end_to_end_dataset_model_fit():
-    np.random.seed(13)
-    ks_data = datasets.KangSchafer(true_ate=10).sample(n_samples=1000)
+    """test fitting data & model"""
+    ks_data = datasets.KangSchafer(true_ate=10, seed=13).sample(n_samples=1000)
     tf.random.set_seed(13)
     model = models.build_toy_model(n_states=5, n_features=ks_data.features.shape[1], compile=True)
     inputs, outputs = ks_data.to_keras_inputs_outputs()
     history = model.fit(
         inputs,
         outputs,
-        epochs=2,
+        epochs=20,
         batch_size=64,
         verbose=2,
         validation_split=0.2,
@@ -77,7 +101,9 @@ def test_end_to_end_dataset_model_fit():
 
     assert preds.shape[0] == ks_data.n_samples
 
-    ate = inference.predict_ate(model, inputs[0])
-    assert ate > 0
+    ute = inference.predict_ute_binary(model, inputs[0])
+    assert ute.shape[0] == ks_data.n_samples
 
-    _ = inference.predict_ute(model, inputs[0])
+    ate = inference.predict_ate_binary(model, inputs[0])
+    print(ate)
+    assert ate == pytest.approx(10.0, 2.0)

pypsps/tests/test_utils.py
@@ -10,6 +10,7 @@ from ..keras import models
 
 
 def test_split_y_does_not_drop_columns():
+    """test split_y"""
     np.random.seed(13)
     ks_data = datasets.KangSchafer(true_ate=10).sample(n_samples=1000)
     tf.random.set_seed(13)
@@ -30,10 +31,11 @@ def test_split_y_does_not_drop_columns():
 
 
 def test_agg_outcome_preds_works():
+    """test aggregating by state works"""
     tf.random.set_seed(0)
     random.seed(0)
     np.random.seed(0)
-    ks_data = datasets.KangSchafer(true_ate=10).sample(n_samples=1000)
+    ks_data = datasets.KangSchafer(true_ate=10, seed=0).sample(n_samples=1000)
     tf.random.set_seed(13)
     n_states = 5
     model = models.build_toy_model(
@@ -66,13 +68,14 @@ def test_agg_outcome_preds_works():
     )
     cor_pred_true = np.corrcoef(avg_outcome_mean, outputs[:, 0])
     print(cor_pred_true)
-    assert cor_pred_true[0, 1] > 0.4
+    assert cor_pred_true[0, 1] > 0.689
 
 
 def test_prepare_keras_inputs_outputs():
+    """test kersa inputs/outputs works"""
     random.seed(0)
     np.random.seed(0)
-    ks_data = datasets.KangSchafer(true_ate=10).sample(n_samples=1000)
+    ks_data = datasets.KangSchafer(true_ate=10, seed=0).sample(n_samples=1000)
 
     res = utils.prepare_keras_inputs_outputs(ks_data.features, ks_data.treatments, ks_data.outcomes)
 